9f37c3986b44154d558086ebbf146ea25b5ef619
aamathew <134253292+aamathew@users.noreply.github.com>
2023-06-26 11:19:42 -0400

    heroku app crashing

--- models/bike.js
+++ models/bike.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
 const bikeSchema = new mongoose.Schema({
   name: {
@@ -21,7 +21,7 @@ const bikeSchema = new mongoose.Schema({
   color: {
     type: String,
     enum: ["Red", "Blue", "Silver", "Black", "White"],
-  },
-});
+  }}, {collection: 'bikerecords'}
+);
 
 module.exports = mongoose.model("Bike", bikeSchema);
